07775c7b6e78eb40040ecc8f2edca1cc11b62b39
Travis Turner <tturner@silabs.com>
2018-01-04 15:45:29 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jenkins_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            greeting(0);
+
+        }
+
+        static void greeting(int message)
+        {
+            if (message == 1)
+                Console.WriteLine("Hello World");
+            if (message == 2)
+                Console.WriteLine("Hello Mars");
+            else
+                Console.WriteLine("No Greeting");
+        }
+    }
+}
